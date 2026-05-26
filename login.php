afcf2f75934f4dcfe6591d07b0d48078a98994a5
Alex Ruiz <torvinchi@gmail.com>
2026-05-26 12:41:51 +0200

    Version estable: codigo html/css terminado excepto reserva. Inicio de php

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Iniciar Sesión - SportCenter</title>
+    <link rel="stylesheet" href="css/estilos-base.css">
+</head>
+<body>
+    <header class="cabecera">
+        <div class="contenedor">
+            <div class="logo">SPORT CENTER</div>
+            <nav class="menu">
+                <a href="index.html">INICIO</a>
+                <a href="login.php" class="pagina-activa">INICIAR SESIÓN</a>
+                <a href="registro.php">REGISTRARTE</a>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div class="bloque-formularios">
+            <div class="caja-principal">
+                <h2 class="titulo-seccion">INICIAR SESIÓN</h2>
+                <form>
+                    <div class="campo"><label>Email / Usuario</label><input type="text"></div>
+                    <div class="campo"><label>Contraseña</label><input type="password"></div>
+                    <a href="principal.html" class="boton">ENTRAR</a>
+                    <a href="recuperar.php" class="boton">RECUPERAR CONTRASERÑA</a>
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
