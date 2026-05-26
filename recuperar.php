cb6704607a316df35b2890c00d0f8ff6617f2589
Alex Ruiz <torvinchi@gmail.com>
2026-05-26 17:36:15 +0200

    Archivos HTML recuperados e integrados en pistas

---
+++ recuperar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Recuperar - SportCenter</title>
+    <link rel="stylesheet" href="css/estilos-base.css">
+</head>
+<body>
+    <header class="cabecera">
+        <div class="contenedor">
+            <div class="logo">SPORT CENTER</div>
+            <nav class="menu">
+                <a href="index.html">INICIO</a>
+                <a href="login.php" class="pagina-activa">INICIAR SESIÓN</a>
+                <a href="registro.php">REGISTRARTE</a>
+            </nav>
+        </div>
+    </header>
+
+    <main> 
+        <div class="bloque-formularios">   
+            <div class="caja-principal">
+                <h2 class="titulo-seccion">RECUPERAR ACCESO</h2>
+                <p class="parrafo-espaciado">Introduce tu correo electrónico para recibir las instrucciones de recuperación.</p>
+                <form>
+                    <div class="campo">
+                        <label>Email de Socio</label>
+                        <input type="email" placeholder="ejemplo@ejemplo.com">
+                    </div>
+                    <a href="login.php" class="boton">ENVIAR ENLACE</a>
+                </form>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
